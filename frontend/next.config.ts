37acffae23daf1c9ec12c7fea7aa60a217f688b0
ki-sd <redringo4@gmail.com>
2026-09-22 10:08:29 +0900

    chore: 프론트엔드 및 Nginx Docker 배포 환경 구성

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+    output:"standalone",
+    
     async rewrites() {
         if(process.env.NODE_ENV!=="development") {
             return [];
